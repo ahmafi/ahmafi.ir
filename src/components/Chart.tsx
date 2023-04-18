8f5ff9af649f9e33d15f0982a6da3e89583ff4c9
Amir Hossein Mafi <amir77mafi@gmail.com>
2023-04-18 19:30:57 +0330

    add mdx support

---
+++ src/components/Chart.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  className: string;
+};
+
+const Chart = ({ className }: Props): JSX.Element => {
+  return (
+    <div>
+      <div>Chart Component</div>
+    </div>
+  );
+};
+
+export default Chart;
